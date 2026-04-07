2b4fee83b762284f218b27fc82ee69dd66ed5bfa
Purr404 <callmewhoosh@gmail.com>
2026-04-07 13:08:49 +0800

    Update AppNavigator.js

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -1,18 +1,14 @@
 import React from 'react';
-import { createStackNavigator } from '@react-navigation/stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createStackNavigator } from '@react-navigation/stack';
 import { useAuth } from '../context/AuthContext';
 
-// Screens (we'll add these next)
 import LoginScreen from '../screens/LoginScreen';
 import RegisterScreen from '../screens/RegisterScreen';
 import HomeScreen from '../screens/HomeScreen';
-import RoomDetailsScreen from '../screens/RoomDetailsScreen';
-import BookingScreen from '../screens/BookingScreen';
-import OwnerDashboardScreen from '../screens/OwnerDashboardScreen';
-import PostListingScreen from '../screens/PostListingScreen';
-import AdminPanelScreen from '../screens/AdminPanelScreen';
-import LocationSelectorScreen from '../screens/LocationSelectorScreen';
+import InboxScreen from '../screens/InboxScreen';
+import ChatScreen from '../screens/ChatScreen';
+import ProfileScreen from '../screens/ProfileScreen';
 
 const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
@@ -21,8 +17,8 @@ function MainTabs() {
   return (
     <Tab.Navigator>
       <Tab.Screen name="Home" component={HomeScreen} />
-      <Tab.Screen name="Owner Dashboard" component={OwnerDashboardScreen} />
-      <Tab.Screen name="Admin Panel" component={AdminPanelScreen} />
+      <Tab.Screen name="Inbox" component={InboxScreen} />
+      <Tab.Screen name="Profile" component={ProfileScreen} />
     </Tab.Navigator>
   );
 }
@@ -42,10 +38,7 @@ export default function AppNavigator() {
       ) : (
         <>
           <Stack.Screen name="Main" component={MainTabs} options={{ headerShown: false }} />
-          <Stack.Screen name="RoomDetails" component={RoomDetailsScreen} />
-          <Stack.Screen name="Booking" component={BookingScreen} />
-          <Stack.Screen name="PostListing" component={PostListingScreen} />
-          <Stack.Screen name="LocationSelector" component={LocationSelectorScreen} />
+          <Stack.Screen name="Chat" component={ChatScreen} />
         </>
       )}
     </Stack.Navigator>
